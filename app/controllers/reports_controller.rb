0511208d0fa01858aef656d3bac431e22a991024
Sammie2022 <samuelnganganganga26@gmail.com>
2025-05-28 16:33:07 +0300

    Add chart report: issues per status and per project using Chartkick

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def charts
-    @issues_by_status = Issue.group(:status).count
-    @issues_by_project = Issue.joins(:project).group("projects.title").count
+    @issues_per_status = Issue.group(:status).count
+    @issues_per_project = Project.includes(:issues).map { |project| [project.title, project.issues.count] }.to_h
   end
 end
