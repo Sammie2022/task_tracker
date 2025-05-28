cab21e429fed2506ea35e29a1541ef15aac4b8a1
Sammie2022 <samuelnganganganga26@gmail.com>
2025-05-28 15:29:02 +0300

    Add Bootstrap-styled views for Projects and Issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,9 @@
 class Issue < ApplicationRecord
   belongs_to :project
+
+  STATUSES = ["New", "In Progress", "Closed"]
+
+  validates :title, presence: true
+  validates :status, presence: true, inclusion: { in: STATUSES }
+  validates :priority, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+  has_many :issues, dependent: :destroy
+  validates :title, presence: true
 end
